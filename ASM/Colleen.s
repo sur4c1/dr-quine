0e8665203f14d39cc5e0addeeff8418454f57841
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2024-07-28 15:54:07 +0200

    working ASM Colleen, not magicalizesd

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,13 +1,70 @@
 extern	printf
+extern	stdout
+extern	fflush
 
 SECTION .text
-global	main
+global	_start
 
-main:
-	push	hello
-	call	printf
-	add		rsp, 8
+noop:
 	ret
 
+_start:
+	mov		rbx,	magic
+
+	loop_start:
+		cmp		byte [rbx],	0
+		je		loop_end
+
+		cmp		byte [rbx], 63
+		jne		question_mark_end
+			mov		rdi,	fmt_str
+			mov		rsi,	magic
+			xor		rax, rax
+			call	printf
+			jmp		end_increment
+		question_mark_end:
+
+		cmp		byte [rbx],	82
+		jne		upper_n_end
+			mov		rdi,	fmt_chr
+			mov		rsi,	10 ;; newline
+			xor		rax, rax
+			call	printf
+			jmp		end_increment
+		upper_n_end:
+
+		cmp		byte [rbx],	39
+		jne		single_quote_end
+			mov		rdi,	fmt_chr
+			mov		rsi,	34 ;;double quotes
+			xor		rax, rax
+			call	printf
+			jmp		end_increment
+		single_quote_end:
+
+		mov		rdi,	fmt_chr
+		xor		rsi,	rsi
+		mov		sil,	byte [rbx]
+		xor		rax, rax
+		call	printf
+
+		end_increment:
+			inc	rbx
+			jmp	loop_start
+	loop_end:
+
+	call	noop
+
+	mov		rdi,	[stdout]
+	call	fflush
+
+	;; exit(0)
+	mov		rax, 60
+	xor		rdi,	rdi
+	syscall
+
 SECTION .data
-hello: db "Hello, world!", 10, 0 ;; 10 is newline, 0 is \0
+magic			db, "?",	0
+fmt_str:		db	"%s",	0
+fmt_chr:		db	"%c",	0
+
