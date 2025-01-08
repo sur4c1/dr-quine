dc8b8212cbd9604ed806ae7c61516279da3aef55
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2025-01-08 14:34:53 +0100

    🧪 ASM: teting all three programs

---
+++ ASM/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+make --silent re
+
+OK="[\033[0;32mOK\033[0m]"
+KO="[\033[0;31mKO\033[0m]"
+
+printf "Colleen: "
+if cmp -s <(./Colleen) Colleen.s; then
+	printf "$OK"
+else
+	printf "$KO"
+fi
+printf "\n"
+
+printf "Grace:   "
+./Grace 2> /dev/null
+if cmp -s Grace_kid.s Grace.s; then
+	printf "$OK"
+else
+	printf "$KO"
+fi
+printf "\n"
+
+mkdir -p sully_test
+cp cp Sully sully_test 2> /dev/null
+cd sully_test
+./Sully 2> /dev/null
+# check is there is 13 files with the name containing Sully
+printf "Sully:   "
+if [ $(ls -1 | grep Sully | wc -l) -eq 13 ]; then
+	printf "$OK"
+else
+	printf "$KO"
+fi
+# check if all the .c files are the same with only the line 'int i = 5;' that changes
+for i in {0..5}; do
+	sed "s/i = $i;/i = 5;/" Sully_$i.s > Sully_$i.s.tmp
+	if ! cmp -s Sully_$i.s.tmp ../Sully.c; then
+		printf "$KO"
+		break
+	else
+		printf "$OK"
+	fi
+done
+cd ..
+rm -rf sully_test
+printf "\n"
